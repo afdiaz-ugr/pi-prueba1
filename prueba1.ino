ee832b3912442c4a5ec7c9d9b1e3b6745137a7d5
Antonio Díaz <afdiaz@ugr.es>
2020-12-16 08:19:38 +0100

    prueba 3

--- prueba1.ino
+++ prueba1.ino
@@ -9,8 +9,8 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(13,HIGH);
-  delay(1500);
+  delay(1200);
   digitalWrite(13,LOW);
-  delay(1500);
-  
+  delay(1200);
+
 }
